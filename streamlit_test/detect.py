2ff6dc5e0221d0eb2f13ed5ceb719a5b3b38169f
chanyub <chanyub1008@naver.com>
2021-06-03 23:47:20 +0900

    pkl test

--- streamlit_test/detect.py
+++ streamlit_test/detect.py
@@ -3,16 +3,23 @@ import os,sys
 import cv2
 import numpy as np
 import pickle
+from PIL import Image, ImageOps
 import pandas as pd
 def predict(image1):
     image1 = np.array(image1)
     with open("./streamlit_test/data/images/image.pkl","wb") as fw:
         pickle.dump(image1,fw)
-    # cv2.imwrite('./streamlit_test/data/images/train_00000.jpg',image1) # must change
-    test = "python ./streamlit_test/inference.py"
-    os.system(test)
-    f = open('./streamlit_test/submit/output.csv', 'r') # must change
-    test = f.read()
-    file_name = test.split('\t')[0]
-    latex_info = test.split('\t')[1:]
-    return latex_info[0]
+
+    with open("./streamlit_test/data/images/image.pkl", "rb") as fr:
+        data = pickle.load(fr)
+        image = Image.fromarray(data)
+
+    return image
+    # # cv2.imwrite('./streamlit_test/data/images/train_00000.jpg',image1) # must change
+    # test = "python ./streamlit_test/inference.py"
+    # os.system(test)
+    # f = open('./streamlit_test/submit/output.csv', 'r') # must change
+    # test = f.read()
+    # file_name = test.split('\t')[0]
+    # latex_info = test.split('\t')[1:]
+    # return latex_info[0]
--- streamlit_test/streamlit_test.py
+++ streamlit_test/streamlit_test.py
@@ -51,7 +51,9 @@ if uploaded_file is not None:
     label = predict(image)
     # st.write('%s (%.2f%%)' % (label[1], label[2]*100))
     # st.write(label)
-    st.markdown('$'+label+'$')
+    st.image(label)
+    st.write(label)
+    # st.markdown('$'+label+'$')
     # latex_to_img(label)
 
 
